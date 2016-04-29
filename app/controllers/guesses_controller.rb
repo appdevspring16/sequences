19dcccb04499f940e36ef0bff75aa9df53a905a1
Hannah <hannah.meyer@chicagobooth.edu>
2016-04-29 16:37:47 -0500

    Second Form
    numbers on the list

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,13 @@
 class GuessesController < ApplicationController
   def index
-    
+    @user_first = params[:first_word]
+    @user_second = params[:second_word]
+    @user_third = params[:third_word]
     render("index.html.erb")
+  end
+
+  def script
+    @user_rule = params[:rule]
+    render("script.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/all_guesses", { :controller=>"guesses", :action=>"index"})
+  get("/show_answer", { :controller=>"guesses", :action =>"script"})
 end
