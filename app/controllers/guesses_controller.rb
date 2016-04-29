c7fe35c63c40a67652de342d9c5c613b60466582
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-04-29 16:54:38 -0500

    answer page working and adding list and database

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,17 @@
 class GuessesController < ApplicationController
   def index
 
+    @user_first = params[:first_number]
+    @user_second = params[:second_number]
+    @user_third = params[:third_number]
+
     render("index.html.erb")
+  end
+
+  def answer
+
+    @user_rule = params[:rule]
+
+    render("answer.html.erb")
   end
 end
