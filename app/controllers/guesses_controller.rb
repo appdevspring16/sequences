e20869ed6e6c0eddf6e9c6c8a8753f55ac7b7575
Emily Gikow <egikow@chicagobooth.edu>
2016-04-29 16:42:44 -0500

    Added first, second, third user inputs

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,19 @@
+class GuessesController < ApplicationController
+
+  def index
+
+    @user_first = params[:first_number]
+    @user_second = params[:second_number]
+    @user_third = params[:third_number]
+
+    render("index.html.erb")
+  end
+
+  def answer
+    # params = {"rule"=>"My answer"}
+    @user_rule = params[:rule]
+
+    render("answer.html.erb")
+  end
+
+end
