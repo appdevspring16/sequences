cd7fa2119919a70f4a6feddc86045dcf43ed23a2
Starr Marcello <starr.marcello@users.noreply.github.com>
2016-04-29 17:04:20 -0500

    Stopping point in class

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,25 @@
 class GuessesController < ApplicationController
   def index
+    g = Guess.new
+
+    g.first = params[:first_number]
+    g.second = params[:second_number]
+    g.third = params[:third_number]
+
+    g.save
+
+    # @user_first = params[:first_number]
+    # @user_second = params[:second_number]
+    # @user_third = params[:third_number]
 
     render("index.html.erb")
+  end
+
+  def answer
+
+    #params = {"rule"=> "My answer"}
+    @user_rule = params[:rule]
+
+    render("answer.html.erb")
   end
 end
