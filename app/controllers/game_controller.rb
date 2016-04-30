cb6317283fd853145b05b024de3b43154c72e8bd
hkerstens <h.kerstens@chicagobooth.edu>
2016-04-30 15:26:00 -0500

    class

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,6 @@
+class GameController < ApplicationController
+def numbers
+
+render("game.html.erb")
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "game", :action => "numbers" })
 end
